f968f195d30b03ef1cbcd1635e38b93a22057326
79101 <sochnevd@list.ru>
2023-01-16 13:38:57 +0300

    change pl vs pl create error

--- domzad5.1.py
+++ domzad5.1.py
@@ -1,9 +1,13 @@
-import random
+from random import randint
 
 name1 = input('Введите имя первого игрока: ')
 name2 = input('Введите имя второго игрока: ')
-igrok = random.randrange(1,3)
+q1 = 0
+q2 = 0
+igrok = randint(1, 2)
 a = int(input('Введите количество конфет: '))
+while a <= 28:
+    a = int(input('Вы ввели неверное количество конфет. Попробуйте заново!'))
 if igrok == 1:
     print('Первый ход делает ', name1)
 else:
@@ -12,10 +16,16 @@ else:
 while a >0:
     if a > 28:
         igrok1 = int(input(f'На столе осталось {a} конфет. Возьмите не больше 28 конфет, сколько конфет взял игрок, {name1} ? '))
-        a = a - igrok1
+        while igrok1 < 0 or igrok1 > 28 or igrok1 > a:
+            igrok1 = int(input('Вы берете неверное количество конфет! Попробуйте снова: '))
+        a -= igrok1
+        q1 += igrok1
         if a > 28:
             igrok2 = int(input(f'На столе осталось {a} конфет. Возьмите не больше 28 конфет, сколько конфет взял игрок, {name2} ? '))
-            a = a - igrok2
+            while igrok2 < 0 or igrok2 > 28 or igrok2 > a:
+                igrok2 = int(input('Вы берете неверное количество конфет! Попробуйте снова: '))
+            a -= igrok2
+            q1 += igrok2
             if a <= 28:
                 if igrok == 1:
                     print('Осталось ', a,' конфет, их забирает и побеждает:', name1)
